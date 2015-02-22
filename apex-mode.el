e81fd30bdabc6701588d6c0fff66cba5e06041a2
Magnus Nyberg <magnus@nexter.se>
2015-02-22 10:11:03 +0100

    Cleanup and indent fix

--- apex-mode.el
+++ apex-mode.el
@@ -1,14 +1,24 @@
-; -*- Mode: Emacs-Lisp -*-
-
 ;;; apex-mode.el -- Major mode for Salesforce Apex files
 
-;; Put this file into your Emacs lisp path (eg. site-lisp)
-;; and add the following line to your ~/.emacs file:
-;;
-;;   (require 'apex-mode)
-(require 'font-lock)
+;; Copyright (C) 2015  Magnus Nyberg
 
-(defvar apex-mode-hook nil)
+;; Author: Magnus Nyberg <magnus@nexter.se>
+;; Keywords: apex, languages, salesforce
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
 
 (defvar apex-mode-map
   (let ((keymap (make-sparse-keymap)))
@@ -17,24 +27,15 @@
   "Keymap for Apex major mode")
 
 (add-to-list 'auto-mode-alist '("\\.cls\\'" . apex-mode))
-
 (add-to-list 'auto-mode-alist '("\\.trigger\\'" . apex-mode))
 
-;; ;; the command to comment/uncomment text
-;; (defun apex-comment-dwim (arg)
-;; "Comment or uncomment current line or region in a smart way.
-;; For detail, see `comment-dwim'."
-;;    (interactive "*P")
-;;    (require 'newcomment)
-;;    (let ((deactivate-mark nil) (comment-start "/*") (comment-end "*/"))
-;;      (comment-dwim arg)))
-
 (define-derived-mode apex-mode java-mode "Apex"
-  "Major mode for editing Salesforce Apex files"
-  ;; (define-key apex-mode-map [remap comment-dwim] 'apex-comment-dwim)
+  "Major mode for editing Salesforce Apex files")
 
-;;  (set (make-local-variable 'font-lock-defaults) '(apex-font-lock-keywords nil t))
-  )
+(defun my-apex-mode-hook ()
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'topmost-intro-cont 0)) ; dirty fix for apex coll initializer
+(add-hook 'apex-mode-hook 'my-apex-mode-hook)
 
 (provide 'apex-mode)
 
